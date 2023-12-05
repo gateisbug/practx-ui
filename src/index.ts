d1df6d359da4e1949de49cc2dcc07fbac18a2449
default <default>
2023-12-05 14:56:47 +0900

    index에 textarea 추가

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { Button, type ButtonProps } from './ui';
 export { Input, type InputProps } from './ui';
 export { InputBox, type InputBoxProps } from './ui';
 export { ScrollView, type ScrollViewProps } from './ui';
+export { Textarea, type TextareaProps } from './ui';
